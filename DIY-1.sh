d1e5a6a72cde62b6f659084cdfca64e9a160920e
flykky <40863329+zqp561022@users.noreply.github.com>
2024-04-03 23:12:46 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -16,6 +16,7 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 
 # 5-添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 #6-添加自用软件包
